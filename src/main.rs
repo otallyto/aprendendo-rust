1c3557f9a469f0356c4acfe32dec294b9a935aca
otallyto <rodrigues.tallyto@hotmail.com>
2021-05-04 21:04:56 -0300

    aprendendo rust

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn soma(x: i32, y: i32) -> i32 {
+    return x + y;
+}
+fn main() {
+    let _a = 1;
+    let _b = 3;
+    let _name = "Tállyto";
+    let _cores = vec!["red", "green"];
+    println!("A soma é igual a {}", soma(4,7));
+}
